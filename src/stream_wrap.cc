31a23e7af730bbe9d40f4580fcee5920e920e8e4
yuebiaochen <yuebiaochen@tencent.com>
2020-08-17 17:28:37 +0800

    'm'

--- src/stream_wrap.cc
+++ src/stream_wrap.cc
@@ -145,13 +145,15 @@ bool LibuvStreamWrap::IsIPCPipe() {
 
 
 int LibuvStreamWrap::ReadStart() {
-  return uv_read_start(stream(), [](uv_handle_t* handle,
-                                    size_t suggested_size,
-                                    uv_buf_t* buf) {
-    static_cast<LibuvStreamWrap*>(handle->data)->OnUvAlloc(suggested_size, buf);
-  }, [](uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
-    static_cast<LibuvStreamWrap*>(stream->data)->OnUvRead(nread, buf);
-  });
+  return uv_read_start(
+    stream(), 
+    [](uv_handle_t* handle,size_t suggested_size,uv_buf_t* buf) {
+      static_cast<LibuvStreamWrap*>(handle->data)->OnUvAlloc(suggested_size, buf);
+    }, 
+    [](uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
+      static_cast<LibuvStreamWrap*>(stream->data)->OnUvRead(nread, buf);
+    }
+  );
 }
 
 
